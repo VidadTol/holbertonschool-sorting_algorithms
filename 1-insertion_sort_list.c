6d53929ab31c62ba12937f82b12acd538bab8ce3
David Tolza <9769@holbertonstudents.com>
2024-12-12 13:14:33 +0100

    insertion sort list modif betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,9 +1,11 @@
 #include "sort.h"
 #include <stdio.h>
 /**
- *swap_nodes - that sorts a doubly linked list of integers 
+ *swap_nodes - that sorts a doubly linked list of integers
  *in ascending order using the Insertion sort algorithm
  *@list: pointer to the list we need to sort
+ *@node1: pointer
+ *@node2: pointer
  *Return: nothing
  */
 
@@ -18,13 +20,20 @@ void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 		*list = node2;
 	}
 	if (node2->next)
-	
+
 	node2->next->prev = node1;
 	node1->next = node2->next;
 	node2->prev = node1->prev;
 	node1->prev = node2;
 	node2->next = node1;
 }
+/**
+ *insertion_sort_list - that sorts a doubly linked list of
+ *integers in ascending order using the Insertion sort algorithm
+ *@list: double pointer to the head of the list
+ *
+ */
+
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *sorted, *next;
@@ -42,8 +51,8 @@ void insertion_sort_list(listint_t **list)
 		current = sorted->prev;
 		while (current != NULL && sorted->n < current->n)
 		{
-			swap_nodes(list, current, sorted); 
-			print_list(*list); 
+			swap_nodes(list, current, sorted);
+			print_list(*list);
 			current = sorted->prev;
 		}
 		sorted = next;
